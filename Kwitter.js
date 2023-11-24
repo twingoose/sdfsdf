66f723d3cfcb07060569d52d1fb553f963cb9657
twingoose <140074426+twingoose@users.noreply.github.com>
2023-11-24 09:20:07 -0500

    Add files via upload

---
+++ Kwitter.js
@@ -0,0 +1,15 @@
+function addUser()
+{
+    user_name = document.getElementById("user_name").value;
+
+localStorage.setItem("user_name",user_name);
+
+window_location = "kwitter_room.html"
+}
+
+function logout()
+{
+    
+}
+
+
